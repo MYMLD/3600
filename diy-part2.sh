ff7017fc7d65b8a56809d757d70f0a9d7ff41bfb
MYMLD <62656690+MYMLD@users.noreply.github.com>
2022-03-01 22:01:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,9 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
- rm -rf tmp && ./scripts/feeds install -f -p helloworld shadowsocksr-libev
+
+./scripts/feeds update helloworld
+./scripts/feeds install -a -f -p helloworld
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
