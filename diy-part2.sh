a47747733495f44e351338fec9b028366278de12
MYMLD <62656690+MYMLD@users.noreply.github.com>
2022-03-01 22:25:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,5 +13,10 @@
 ./scripts/feeds update helloworld
 ./scripts/feeds install -a -f -p helloworld
 
+sed -i "/helloworld/d" "feeds.conf.default"
+./scripts/feeds clean
+./scripts/feeds update -a
+./scripts/feeds install -a
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
