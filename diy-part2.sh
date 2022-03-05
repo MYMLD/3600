c0f44a6c360c1b62d1f304a635d7b688de89273f
MYMLD <62656690+MYMLD@users.noreply.github.com>
2022-03-05 23:34:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,11 +21,6 @@ git submodule add -f --name helloworld https://github.com/fw876/helloworld.git p
 
 git submodule update --remote package/helloworld
 
-mkdir -p package/helloworld
-for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
-  svn checkout "https://github.com/immortalwrt/packages/trunk/net/$i" "package/helloworld/$i"; \
-done
-
 
 
 # Modify default IP
