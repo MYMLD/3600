59c3e48f5c60b1b4bf1049ebd3cd4f84048749fd
MYMLD <62656690+MYMLD@users.noreply.github.com>
2022-03-11 10:18:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,11 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+rm -rf package/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git -C package/helloworld pull
+git clone --depth=1 https://github.com/kenzok8/small.git package
+git  clone --depth=1 openwrt-packag https://github.com/MYMLD/openwrt-packag.git package
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
