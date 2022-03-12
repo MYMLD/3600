d5d163a87316f91873e764d74cd7d36ddaeb83f1
MYMLD <62656690+MYMLD@users.noreply.github.com>
2022-03-12 13:31:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,5 +20,11 @@ git -C package/helloworld pull
 git  clone --depth=1 https://github.com/MYMLD/openwrt-packag.git package/openwrt-packag
 git -C package/openwrt-packag pull
 
+svn checkout https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
+svn checkout https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
+
+sed -i 'N;24a\tools-y += ucl upx' tools/Makefile
+sed -i 'N;40a\$(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
